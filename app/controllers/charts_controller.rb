9885ba799e16296c9b83e731406a187bd8b84cb2
Mitch Merrell <mitchmerrell88@gmail.com>
2018-03-01 15:08:26 -0700

    song column

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,28 @@
 class ChartsController < ApplicationController
+  before_action :set_chart, only: [:show, :update, :edit, :destroy]
+
   def index
+    @charts = Chart.all
   end
 
   def show
   end
 
   def new
+    @charts = Chart.new
   end
 
   def edit
   end
+
+  def update
+    
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+  
 end
--- db/migrate/20180301214500_create_songs.rb
+++ db/migrate/20180301214500_create_songs.rb
@@ -2,6 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.1]
   def change
     create_table :songs do |t|
       t.string :title
+      t.integer :sales
       t.belongs_to :artist, foreign_key: true
 
       t.timestamps
