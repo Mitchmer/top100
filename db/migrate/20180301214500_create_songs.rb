4a17b57f3750b2549dd36ba28ea5c98d35d8af34
Mitch Merrell <mitchmerrell88@gmail.com>
2018-03-01 16:20:24 -0700

    figuring out git

--- db/migrate/20180301214500_create_songs.rb
+++ db/migrate/20180301214500_create_songs.rb
@@ -2,7 +2,9 @@ class CreateSongs < ActiveRecord::Migration[5.1]
   def change
     create_table :songs do |t|
       t.string :title
+      t.string :genre
       t.integer :sales
+
       t.belongs_to :artist, foreign_key: true
       t.belongs_to :chart
 
