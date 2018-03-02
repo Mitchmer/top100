49c56b75ff00cb889f68e0c6f8abd057ec83c7ff
Chris Watkin <chriswatkin@gmail.com>
2018-03-01 17:00:26 -0700

    song controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -66,7 +66,7 @@ class SongsController < ApplicationController
 
     def song_params
       
-      params.require(:song).permit(:title, :sales)
+      params.require(:song).permit(:title, :sales, :genre)
 
     end
 >>>>>>> create song controller
