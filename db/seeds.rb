51003c3b9cf3f2ee291f76d366aea697f8815444
Reid Laitinen <reidlaitinen@gmail.com>
2018-03-01 22:17:38 -0700

    fix seeds, charts/index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Chart.create(name: "Top 100 K-Pop")
-Chart.create(name: "Top 100 Rock")
+Chart.create(name: "K-Pop")
+Chart.create(name: "Rock")
 
 
 20.times do |i|
@@ -22,11 +22,11 @@ end
 # Generate songs for k-pop artists
 200.times do |i|
   artist_id = Artist.find_by_id(rand(1..20)).id
-  Song.create(title: Faker::Pokemon.move, artist_id: artist_id, chart_id: 1, sales: rand(1000..10000))
+  Song.create(title: Faker::Pokemon.move, artist_id: artist_id, chart_id: 1, genre: "K-Pop", sales: rand(1000..10000))
 end
 
 # Generate songs for rock artists
 200.times do |i|
   artist_id = Artist.find_by_id(rand(21..40)).id
-  Song.create(title: Faker::Ancient.hero, artist_id: artist_id, chart_id: 2, sales: rand(1000..10000))
+  Song.create(title: Faker::Ancient.hero, artist_id: artist_id, chart_id: 2, genre: "Rock", sales: rand(1000..10000))
 end
