2569fb50ef7916c39f89c5dce5a9fc8c3658cc78
Reid Laitinen <reidlaitinen@gmail.com>
2018-03-02 09:33:00 -0700

    new song route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
+  get '/songs/new', to: 'songs#new'
 
   resources :artists do
     resources :songs
